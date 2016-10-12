80370f95ecf0a9698f2f76b3113fd6d96010a7b5
adam <pawelecadam@gmail.com>
2016-10-12 14:35:46 -0400

    Initial Commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,48 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+//= require material.min
+
+$(document).ready(function(){
+    $("body").on( "click", ".start-button", function() {
+        $(".section-one").addClass("hide");
+        $(".section-two").removeClass("hide");
+    });
+    $("body").on( "click", ".q1next-button", function() {
+        $(".section-two").addClass("hide");
+        $(".section-three").removeClass("hide");
+    });
+    $("body").on( "click", ".q2next-button", function() {
+        $(".section-three").addClass("hide");
+        $(".section-four").removeClass("hide");
+    });
+    $("body").on( "click", ".q3next-button", function() {
+        $(".section-four").addClass("hide");
+    });
+
+    $("body").on( "click", ".q1prev-button", function() {
+        $(".section-two").addClass("hide");
+        $(".section-one").removeClass("hide");
+    });
+    $("body").on( "click", ".q2prev-button", function() {
+        $(".section-three").addClass("hide");
+        $(".section-two").removeClass("hide");
+    });
+    $("body").on( "click", ".q3prev-button", function() {
+        $(".section-four").addClass("hide");
+        $(".section-three").removeClass("hide");
+    });
+});
